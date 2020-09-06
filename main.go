13749515e7c1e001041020186cfe1c3f3d76afbf
parkingcams <55210848+parkingcams@users.noreply.github.com>
2020-09-06 09:19:41 -0600

    http hello world

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
-func main(){
-	fmt.Print('wasssup')
+func main() {
+    http.HandleFunc("/", HelloServer)
+    http.ListenAndServe(":3030", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
